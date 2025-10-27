3b93bf1ca48f84ec582d8d2ede2bdbf8790d45e7
bin448482 <zb0618@live.cn>
2025-10-27 22:30:46 +0800

    更新配置库

---
+++ tarot-ai-generator/更新配置库.sql
@@ -0,0 +1,15 @@
+-- SQLite
+-- DELETE FROM dimension WHERE id IN (101);
+
+-- DELETE FROM dimension_translation WHERE dimension_id IN (101);
+
+select * from card_interpretation_dimension where dimension_id = 109;
+
+-- DELETE FROM dimension WHERE id >= 95;
+-- DELETE FROM dimension_translation WHERE dimension_id >= 95;
+
+-- SELECT description from dimension where id = 107;
+
+-- UPDATE dimension
+-- SET description = '发生的事情揭示哪些隐喻，对未来有什么影响？'
+-- WHERE id >= 107;
