6ce89c80dcb396e4875d927158a57c2bae633ae9
bin448482 <zb0618@live.cn>
2025-10-28 11:04:40 +0800

    更新配置库，并且人工更新修复了时间戳在解读内容的问题。

--- tarot-ai-generator/更新配置库.sql
+++ tarot-ai-generator/更新配置库.sql
@@ -3,13 +3,13 @@
 
 -- DELETE FROM dimension_translation WHERE dimension_id IN (101);
 
-select * from card_interpretation_dimension where dimension_id = 109;
+DELETE from card_interpretation_dimension where dimension_id >= 110;
+DELETE from card_interpretation_dimension_translation where dimension_interpretation_id not in (select id from card_interpretation_dimension);
 
 -- DELETE FROM dimension WHERE id >= 95;
 -- DELETE FROM dimension_translation WHERE dimension_id >= 95;
 
 -- SELECT description from dimension where id = 107;
 
--- UPDATE dimension
--- SET description = '发生的事情揭示哪些隐喻，对未来有什么影响？'
--- WHERE id >= 107;
+-- DELETE FROM card_interpretation_dimension_translation
+-- WHERE dimension_id = 112;
